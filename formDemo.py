12c96136fbd212850dd4e2d54d1d075d656b8479
FDani10 <danielfarkas465@gmail.com>
2024-04-30 00:52:24 +0200

    Design
    Alap design létrehozva

--- formDemo.py
+++ formDemo.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import Image,ImageTk
+import ctypes
 
 class board:
     def __init__(self, n11, n12, n13, n21, n22, n23, n31, n32, n33):
@@ -15,155 +17,234 @@ class board:
     def __str__(self):
         return f"{self.r11} | {self.r12} | {self.r13}\n{self.r21} | {self.r22} | {self.r23}\n{self.r31} | {self.r32} | {self.r33}\n"
 
+def charOnLabel(char,om):
+    text = Label(om,text=f"{variables[char].get()}",font=('Ariel',25),bg="#FFBCBC",justify="center")
+    text.place(x=30,y=15,width=18,height=40)
+
+def numOnLabel(char,om):
+    text = Label(om,text=f"{variables[char].get()}",font=('Ariel',25),bg="#AEFFA7",justify="center")
+    text.place(x=30,y=15,width=18,height=40)
+
+def whiteOnLabel(char,om):
+    text = Label(om,text=f"{variables[char].get()}",font=('Ariel',25),bg="#FFFFFF",justify="center")
+    text.place(x=30,y=15,width=18,height=40)
+
 boardsArray = []
 root = Tk()
 
+root.geometry("670x714")
+root["bg"] = "#BBFBFF"
+
 # Define options
-chars = ['/', '*', '-', '+']  # Example values, replace with your options
-numbers = [0,1,2,3,4,5,6,7,8,9]  # Example values, replace with your options
+chars = [' ','/', '*', '-', '+']
+numbers = [' ',0,1,2,3,4,5,6,7,8,9]
+
+image=Image.open(f'./pics/grayBlock.png')
+grayBlock=ImageTk.PhotoImage(image)
+image2=Image.open(f'./pics/pinkCircle.png')
+pickCircle=ImageTk.PhotoImage(image2)
+image3=Image.open(f'./pics/whiteBlock.png')
+whiteBlock=ImageTk.PhotoImage(image3)
+image4=Image.open(f'./pics/greenBlock.png')
+greenBlock=ImageTk.PhotoImage(image4)
 
 # ROW 1
 variables = []
-for i in range(2):
-    inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-    inputtxt.grid(column=i*2+1, row=1, padx=10, pady=10)
-
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *chars)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+2, row=1, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-inputtxt.grid(column=5, row=1, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5)
-inputtxt.tag_configure("center", justify=CENTER)
-inputtxt.insert(INSERT, "=")
-inputtxt.tag_add("center", "1.0", "end")
-inputtxt.config(state=DISABLED)
-inputtxt.grid(column=6, row=1, padx=10, pady=10)
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=20,y=10)
 
 variable = StringVar(root)
 variable.set(' ')
 variables.append(variable)
-om = OptionMenu(root, variable, *numbers)
-om.config(width=6, height=3, bg="lightgray")
-om.grid(column=7, row=1, padx=10, pady=10)
+om1 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(0,om1))
+om1.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om1.place(x=108,y=10)
 
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=196,y=10)
 
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om2 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(1,om2))
+om2.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om2.place(x=284,y=10)
+
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=372,y=10)
+
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=460,y=10,width=50)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz1 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(2,sz1))
+sz1.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz1.place(x=520,y=10)
 
 # ROW 2
-for i in range(3):
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *chars)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+1, row=2, padx=10, pady=10)
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om3 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(3,om3))
+om3.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om3.place(x=20,y=108)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om4 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(4,om4))
+om4.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om4.place(x=196,y=108)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om5 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(5,om5))
+om5.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om5.place(x=372,y=108)
 
 
 # ROW 3
-for i in range(2):
-    if i != 1:
-        inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-        inputtxt.grid(column=i*2+1, row=3, padx=10, pady=10)
-    else:
-        variable = StringVar(root)
-        variable.set(' ')
-        variables.append(variable)
-        om = OptionMenu(root, variable, *numbers)
-        om.config(width=6, height=3, bg="lightgray")
-        om.grid(column=i*2+1, row=3, padx=10, pady=10)
-
-
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *chars)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+2, row=3, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-inputtxt.grid(column=5, row=3, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5)
-inputtxt.tag_configure("center", justify=CENTER)
-inputtxt.insert(INSERT, "=")
-inputtxt.tag_add("center", "1.0", "end")
-inputtxt.config(state=DISABLED)
-inputtxt.grid(column=6, row=3, padx=10, pady=10)
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=20,y=196)
 
 variable = StringVar(root)
 variable.set(' ')
 variables.append(variable)
-om = OptionMenu(root, variable, *numbers)
-om.config(width=6, height=3, bg="lightgray")
-om.grid(column=7, row=3, padx=10, pady=10)
+om6 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(6,om6))
+om6.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om6.place(x=108,y=196)
 
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+wb = OptionMenu(root, variable, *numbers, command= lambda e:whiteOnLabel(7,wb))
+wb.config(image=whiteBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+wb.place(x=196,y=196)
 
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om7 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(8,om7))
+om7.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om7.place(x=284,y=196)
+
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=372,y=196)
+
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=460,y=196,width=50)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz2 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(9,sz2))
+sz2.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz2.place(x=520,y=196)
 
 # ROW 4
-for i in range(3):
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *chars)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+1, row=4, padx=10, pady=10)
-
-
-
-
-# ROW 5
-for i in range(2):
-    inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-    inputtxt.grid(column=i*2+1, row=5, padx=10, pady=10)
-
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *chars)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+2, row=5, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5, state=DISABLED)
-inputtxt.grid(column=5, row=5, padx=10, pady=10)
-
-inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5)
-inputtxt.tag_configure("center", justify=CENTER)
-inputtxt.insert(INSERT, "=")
-inputtxt.tag_add("center", "1.0", "end")
-inputtxt.config(state=DISABLED)
-inputtxt.grid(column=6, row=5, padx=10, pady=10)
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om8 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(10,om8))
+om8.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om8.place(x=20,y=284)
 
 variable = StringVar(root)
 variable.set(' ')
 variables.append(variable)
-om = OptionMenu(root, variable, *numbers)
-om.config(width=6, height=3, bg="lightgray")
-om.grid(column=7, row=5, padx=10, pady=10)
+om9 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(11,om9))
+om9.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om9.place(x=196,y=284)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om10 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(12,om10))
+om10.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om10.place(x=372,y=284)
+
+# ROW 5
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=20,y=372)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om11 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(13,om11))
+om11.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om11.place(x=108,y=372)
+
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=196,y=372)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+om12 = OptionMenu(root, variable, *chars, command= lambda e:charOnLabel(14,om12))
+om12.config(image=pickCircle,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+om12.place(x=284,y=372)
+
+inputtxt = Label(root, image=grayBlock)
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=372,y=372)
+
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=460,y=372,width=50)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz3 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(15,sz3))
+sz3.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz3.place(x=520,y=372)
 
 
 # ROW 6
-for i in range(3):
-    inputtxt = Text(root, height=3, width=6, bg="lightgray", borderwidth=5)
-    inputtxt.tag_configure("center", justify=CENTER)
-    inputtxt.insert(INSERT, "=")
-    inputtxt.tag_add("center", "1.0", "end")
-    inputtxt.config(state=DISABLED)
-    inputtxt.grid(column=i*2+1, row=6, padx=10, pady=10)
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=35,y=460,width=50,height=25)
 
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=211,y=460,width=50,height=25)
+
+inputtxt = Label(root, text="=",font=('Ariel',50),justify="center")
+inputtxt["bg"] = "#BBFBFF"
+inputtxt.place(x=387,y=460,width=50,height=25)
 # ROW 7
-for i in range(3):
-    variable = StringVar(root)
-    variable.set(' ')
-    variables.append(variable)
-    om = OptionMenu(root, variable, *numbers)
-    om.config(width=6, height=3, bg="lightgray")
-    om.grid(column=i*2+1, row=7, padx=10, pady=10)
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz4 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(16,sz4))
+sz4.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz4.place(x=20,y=500)
 
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz5 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(17,sz5))
+sz5.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz5.place(x=196,y=500)
+
+variable = StringVar(root)
+variable.set(' ')
+variables.append(variable)
+sz6 = OptionMenu(root, variable, *numbers, command= lambda e:numOnLabel(18,sz6))
+sz6.config(image=greenBlock,border=0,indicatoron=False,font=('Ariel',20),bg="#BBFBFF",highlightthickness=0,activebackground="#BBFBFF")
+sz6.place(x=372,y=500)
 
 
 # Solver
@@ -266,7 +347,6 @@ def szamolj():
     boardsArray = []
     megvan = True
     for i in range(len(variables)):
-        print(f"{i}: {variables[i].get()}")
         if variables[i].get() == " ":
             megvan = False
 
@@ -286,8 +366,14 @@ def szamolj():
             print(f"{i+1}:")
             print(boardsArray[i])
     else:
-        print("Tölts ki minden mezőt!")
+        ctypes.windll.user32.MessageBoxW(0, "Töltsön ki minden mezőt!", "Hiba!", 0)
 
-btn = Button(root, text="Nyomod fasz xd", command=szamolj)
-btn.grid(column=4,row=8,padx=10,pady=10)
+image_btn=Image.open(f'./pics/megoldasBtn.png')
+solveBtn=ImageTk.PhotoImage(image_btn)
+btn = Button(root, command=szamolj, image=solveBtn)
+btn["bg"] = "#BBFBFF"
+btn["activebackground"] = "#BBFBFF"
+btn["border"] = "0"
+btn.place(x=500,y=500)
+
 root.mainloop()
